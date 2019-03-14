4e9960630ecc06356c61556c8d1c20237fda86f9
JoannaAbella <Joanna.Abella96@gmail.com>
2019-03-14 21:40:58 +0100

    Small adjustment

--- src/login.js
+++ src/login.js
@@ -36,7 +36,7 @@ class Login extends Component {
      <div className='login-window login-tail'>  
         <form className='login-form'>
           <h1 className='login-welcome'>Talkative</h1>
-          <label className='login-title' htmlFor='username'>Pick your username:</label>  
+          <label className='login-title' htmlFor='username'>Pick your username</label>  
           <p className='login-error' style={{ color: this.state.color }}>{this.state.infoMessage}</p>
           <input className='userfield' name='pickusername' onChange={this.onChange} value={this.state.username} name='username' placeholder='Username' spellCheck='false'></input>  
           <label className='userinput' htmlFor='pickusername'></label>  
